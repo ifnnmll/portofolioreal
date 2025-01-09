dc89a576b10f36ec6dabf8f32f305cacaf2501fe
irfancipinang <muhammadirfanm.2006@gmail.com>
2025-01-09 15:18:13 +0700

    done nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from './components/Navbar.jsx'
+import Hero from './components/hero/Hero.jsx'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
