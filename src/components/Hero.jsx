8b7f568e6286eef03258abc53a99ceedc3f58cbf
irfancipinang <muhammadirfanm.2006@gmail.com>
2025-01-13 11:04:03 +0700

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,10 @@ const Hero = () => {
       <h1>Hallo, Saya Muhammad Irfan Maulana</h1>
       <p>Web Developer</p>
       <p>Menjadi Programmer Adalah Impian Saya Sejak Masih Di Bangku SMP</p>
-      <button>Download CV</button>
+      <div className='hero-action'>
+      <div className='hero-connect'>Connect With Me</div>
+      <div className='hero-resume'>my resume</div>
+      </div>
     </div>
   )
 }
